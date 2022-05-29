8222a57c06b78004c0dd0abe45a23632c3214233
jezkovamonika <106450507+jezkovamonika@users.noreply.github.com>
2022-05-29 21:42:14 +0200

    Update web_scraper.js

--- web_scraper.js
+++ web_scraper.js
@@ -33,3 +33,35 @@ async function pageFunction(context) {
     };
 }
 }
+
+
+
+// setting for cheerio scraper
+
+async function pageFunction(context) {
+    const { $, request, log } = context;
+    // The "$" property contains the Cheerio object which is useful
+    // for querying DOM elements and extracting data from them.
+    const pageTitle = $('div.article__title').first().text();
+    const pageDate = $('div.article__info-date').text().trim();
+    const pageOpener = $('h1.article__second-title').first().text().trim();
+    const fullText = $(".article__text").text();
+
+  // The "request" property contains various information about the web page loaded. 
+    const url = request.url;
+    
+    // Use "log" object to print information to actor log.
+    log.info('Page scraped', { url, pageTitle, fullText });
+
+    // Return an object with the data extracted from the page.
+    // It will be stored to the resulting dataset.
+if((fullText.toLowerCase()).includes('украин')) {
+    return {
+        url,
+        pageTitle,
+        pageDate,
+        pageOpener,
+        fullText
+    };
+}
+}
